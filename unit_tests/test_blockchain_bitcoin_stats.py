cf951396c30c399c686a2e1b378adfde7c7b30ea
Lars Brebeck <lbrebeck@pmone.com>
2022-04-28 14:44:21 +0200

    unit tests 1

--- unit_tests/test_blockchain_bitcoin_stats.py
+++ unit_tests/test_blockchain_bitcoin_stats.py
@@ -1,25 +1,30 @@
 #import external modules
 import unittest
-
-from api_endpoints.blockchair_bitcoin_stats import BitcoinStats
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../api_endpoints'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 #import internal classes
-#from api_endpoints.blockchair_bitcoin_stats import BitcoinStats
+from blockchair_bitcoin_stats import BitcoinStats
 
-class TestBitcoinStats(unittest.Testcase): 
+class TestBitcoinStats(unittest.TestCase): 
 
     def test_getUrl(self):
         BitcoinStats().getUrl()
-        self.assertEquals(BitcoinStats().getUrl)
+        self.assertEquals(BitcoinStats().getUrl(), 'https://api.blockchair.com/bitcoin/stats')
 
-    def test_getDbTableName(self):
-        BitcoinStats().getDbTableName
-        self.assertEquals(BitcoinStats()._getDbTableName)
+    #def test_getDbTableName(self):
+    #    BitcoinStats().getDbTableName
+    #    self.assertEquals(BitcoinStats()._getDbTableName)
+#
+    #def test_getDbTableAttributes(self):
+    #    BitcoinStats().getDbTableAttributes
+    #    self.assertEquals(BitcoinStats().getDbTableAttributes)
+#
+    #def test_getDbDynamicInsertPlaceholders(self):
+    #    BitcoinStats().getDbDynamicInsertPlaceholders
+    #    self.assertEquals(BitcoinStats().getDbDynamicInsertPlaceholders)
 
-    def test_getDbTableAttributes(self):
-        BitcoinStats().getDbTableAttributes
-        self.assertEquals(BitcoinStats().getDbTableAttributes)
-
-    def test_getDbDynamicInsertPlaceholders(self):
-        BitcoinStats().getDbDynamicInsertPlaceholders
-        self.assertEquals(BitcoinStats().getDbDynamicInsertPlaceholders)
+if __name__ == '__main__':
+    unittest.main()
