cf245579391f89d5c9886a8684b6f6262e11be6a
bellaxdf97 <isabellalambirth@googlemail.com>
2022-04-29 11:55:46 +0200

    test6

--- unit_tests/test_wavescap_ethereum_bitcoin.py
+++ unit_tests/test_wavescap_ethereum_bitcoin.py
@@ -3,9 +3,9 @@ import unittest
 
 #import internal folder
 import sys, os
-testdir = os.path.dirname(__file__)
-srcdir = '../api_endpoints'
-sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
+pathToApiEndpointsDir = os.path.join(os.path.dirname(__file__), '../api_endpoints')
+sys.path.append(pathToApiEndpointsDir)
 
 #import internal classes
 from wavescap_ethereum_bitcoin import EthereumBitcoinMarketPrices
