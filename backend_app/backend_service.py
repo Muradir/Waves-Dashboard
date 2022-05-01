89c3d38d226da8da358d6d4e4107abcb65d79e90
Lars Brebeck <lbrebeck@pmone.com>
2022-05-01 18:28:26 +0200

    change backend service

--- backend_app/backend_service.py
+++ backend_app/backend_service.py
@@ -1,7 +1,7 @@
 #import internal classes
 from data_processing.crypto_details_processing import CryptoStatsDataProcessing
 from data_processing.market_prices_processing import MarketPricesDataProcessing
-#from data_processing.create_dataframes import CreateDataFrames
+from data_processing.create_dataframes import CreateDataFrames
 
 
 
@@ -10,14 +10,13 @@ class Main:
     #main method of backend service programm
     def main():
         print("Wir schaffen das!")
-<<<<<<< HEAD
         MarketPricesDataProcessing().start()
         CryptoStatsDataProcessing().start()
         #CreateDataFrames.getDataframeUSD()
         #CreateDataFrames.getDataframeBTC()
         #CreateDataFrames.getDataframeETH()
         #CreateDataFrames.getDataframeWAVES()
-=======
+
         #MarketPricesDataProcessing().start()
         #CryptoStatsDataProcessing().start()
         CreateDataFrames.getDataframeUSD()
@@ -25,7 +24,6 @@ class Main:
         CreateDataFrames.getDataframeETH()
         CreateDataFrames.getDataframeWAVES()
         CreateDataFrames.getDataframeCurrency()
->>>>>>> 68e4b7cdf16e9e2eee89b9b79726f3bce4a31247
 
     #invokes the main method
     if __name__ == "__main__":
